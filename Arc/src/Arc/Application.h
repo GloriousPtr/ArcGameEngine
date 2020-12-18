398fddb82b6775611780571c6a31984001def51e
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-18 20:28:29 +0530

    Triangle without shader

--- Arc/src/Arc/Application.h
+++ Arc/src/Arc/Application.h
@@ -33,6 +33,8 @@ namespace ArcEngine
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
--- Arc/src/Platform/OpenGL/OpenGLContext.cpp
+++ Arc/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace ArcEngine
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		ARC_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		ARC_CORE_INFO("OpenGL Info:");
+		ARC_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		ARC_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		ARC_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
