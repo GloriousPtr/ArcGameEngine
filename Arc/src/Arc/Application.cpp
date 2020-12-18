feb1779e1c44f390d8d662f9d9ab234e7f7dc5a5
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-18 21:35:05 +0530

    Added Shader

--- Arc/src/Arc/Application.cpp
+++ Arc/src/Arc/Application.cpp
@@ -40,6 +40,35 @@ namespace ArcEngine
 
 		unsigned int indices[3] = { 0, 1, 2 };
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		std::string vertexSource = R"(
+			#version 330 core
+
+			layout(location = 0) in vec3 a_Position;
+			out vec3 v_Position;
+		
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);
+			}
+		
+		)";
+
+		std::string fragmentSource = R"(
+			#version 330 core
+
+			layout(location = 0) out vec4 color;
+			in vec3 v_Position;
+		
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+			}
+		
+		)";
+		
+		m_Shader.reset(new Shader(vertexSource, fragmentSource));
 	}
 
 	Application::~Application()
@@ -78,6 +107,7 @@ namespace ArcEngine
 			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
+			m_Shader->Bind();
 			glBindVertexArray(m_VertexArray);
 			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 			
--- Arc/src/Arc/Application.h
+++ Arc/src/Arc/Application.h
@@ -8,6 +8,8 @@
 
 #include "Arc/ImGui/ImGuiLayer.h"
 
+#include "Arc/Renderer/Shader.h"
+
 namespace ArcEngine
 {
 	class ARC_API Application
@@ -35,6 +37,7 @@ namespace ArcEngine
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
