67809c0051aafff471d4a275b7fbcd11a38a6e88
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-26 01:29:47 +0530

    Added file reading check

--- Arc/src/Platform/OpenGL/OpenGLShader.cpp
+++ Arc/src/Platform/OpenGL/OpenGLShader.cpp
@@ -126,10 +126,18 @@ namespace ArcEngine
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			const size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+				ARC_CORE_ERROR("Could not read from file '{0}'", filepath);
+			}
 		}
 		else
 		{
