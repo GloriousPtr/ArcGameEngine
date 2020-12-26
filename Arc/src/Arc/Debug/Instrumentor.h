326553936577327aafab504c8caa95618f796572
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-26 13:39:16 +0530

    Fixed Instrumentor generating same start time

--- Arc/src/Arc/Debug/Instrumentor.h
+++ Arc/src/Arc/Debug/Instrumentor.h
@@ -3,15 +3,20 @@
 #include <algorithm>
 #include <chrono>
 #include <fstream>
+#include <iomanip>
 #include <string>
 #include <thread>
 
 namespace ArcEngine
 {
+	using FloatingPointMicroseconds = std::chrono::duration<double, std::micro>;
+	
 	struct ProfileResult
 	{
 	    std::string Name;
-	    long long Start, End;
+	    
+		FloatingPointMicroseconds Start;
+		std::chrono::microseconds ElapsedTime;
 	    std::thread::id ThreadID;
 	};
 
@@ -49,6 +54,7 @@ namespace ArcEngine
 			}
 	    	
 	        m_OutputStream.open(filepath);
+	    	
 	        if (m_OutputStream.is_open()) 
 			{
 				m_CurrentSession = new InstrumentationSession({name});
@@ -76,14 +82,15 @@ namespace ArcEngine
 	        std::string name = result.Name;
 	        std::replace(name.begin(), name.end(), '"', '\'');
 
+	    	json << std::setprecision(3) << std::fixed;
 	        json << ",{";
 			json << "\"cat\":\"function\",";
-			json << "\"dur\":" << (result.End - result.Start) << ',';
+			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
 			json << "\"name\":\"" << name << "\",";
 			json << "\"ph\":\"X\",";
 			json << "\"pid\":0,";
 			json << "\"tid\":" << result.ThreadID << ",";
-			json << "\"ts\":" << result.Start;
+			json << "\"ts\":" << result.Start.count();
 			json << "}";
 
 	        std::lock_guard lock(m_Mutex);
@@ -133,7 +140,7 @@ namespace ArcEngine
 	    InstrumentationTimer(const char* name)
 	        : m_Name(name), m_Stopped(false)
 	    {
-	        m_StartTimepoint = std::chrono::high_resolution_clock::now();
+	    	m_StartTimepoint = std::chrono::steady_clock::now();
 	    }
 
 	    ~InstrumentationTimer()
@@ -144,18 +151,17 @@ namespace ArcEngine
 
 	    void Stop()
 	    {
-	        auto endTimepoint = std::chrono::high_resolution_clock::now();
+			auto endTimepoint = std::chrono::steady_clock::now();
+			auto highResStart = FloatingPointMicroseconds{ m_StartTimepoint.time_since_epoch() };
+			auto elapsedTime = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch() - std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch();
 
-	        long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-	        long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-
-	        Instrumentor::Get().WriteProfile({ m_Name, start, end, std::this_thread::get_id()});
+	        Instrumentor::Get().WriteProfile({ m_Name, highResStart, elapsedTime, std::this_thread::get_id() });
 
 	        m_Stopped = true;
 	    }
 	private:
 	    const char* m_Name;
-	    std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+	    std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
 	    bool m_Stopped;
 	};
 }
