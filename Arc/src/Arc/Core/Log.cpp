7874bd79abd86f4d6ba0440f72f3a9f84cc8143b
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-26 13:50:40 +0530

    Log messages are now additionally sent to ArcEngine.log file
    - changed to use sinks
    - just one console sink
    - one sink and one log file only, different patterns
    - changed pattern
    - added the *.log to .gitignore
    - added timestamps to the log file and set flush level.

--- Arc/src/Arc/Core/Log.cpp
+++ Arc/src/Arc/Core/Log.cpp
@@ -2,6 +2,7 @@
 #include "Arc/Core/Log.h"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace ArcEngine
 {
@@ -10,12 +11,21 @@ namespace ArcEngine
 	
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		
-		s_CoreLogger = spdlog::stdout_color_mt("ARC_ENGINE");
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("ArcEngine.log", true));
+
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("ARC_ENGINE", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
 		
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
