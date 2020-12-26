ef5ac735536da76c028aad875c5b253ac42b997d
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-26 20:10:24 +0530

    Imgui handling mouse and keyboards events

--- Arc/src/Arc/Core/Application.cpp
+++ Arc/src/Arc/Core/Application.cpp
@@ -58,9 +58,10 @@ namespace ArcEngine
 
 		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
 		{
-			(*it)->OnEvent(e);
 			if (e.Handled)
 				break;
+			
+			(*it)->OnEvent(e);
 		}
 	}
 
--- Arc/src/Arc/ImGui/ImGuiLayer.cpp
+++ Arc/src/Arc/ImGui/ImGuiLayer.cpp
@@ -61,6 +61,13 @@ namespace ArcEngine
 		ImGui::DestroyContext();
 	}
 
+	void ImGuiLayer::OnEvent(Event& e)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		e.Handled |= e.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
+		e.Handled |= e.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
+	}
+
 	void ImGuiLayer::Begin()
 	{
 		ARC_PROFILE_FUNCTION();
