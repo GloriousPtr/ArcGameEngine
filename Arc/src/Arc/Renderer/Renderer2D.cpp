e9dbae91415442f0e5574fd93b284cb26a82202a
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-26 20:06:54 +0530

    delete Renderer2D s_Data.QuadVertexBufferBase on shutdown

--- Arc/src/Arc/Renderer/Renderer2D.cpp
+++ Arc/src/Arc/Renderer/Renderer2D.cpp
@@ -106,6 +106,8 @@ namespace ArcEngine
 	void Renderer2D::Shutdown()
 	{
 		ARC_PROFILE_FUNCTION();
+
+		delete[] s_Data.QuadVertexBufferBase;
 	}
 
 	void Renderer2D::BeginScene(const OrthographicCamera& camera)
