778de22b976e7229657a42f7e18ea12ee1dee026
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-26 11:59:27 +0530

    Missing iterator wrappers in LayerStack.h
    Adding const and reverse iterator wrappers

--- Arc/src/Arc/Core/Application.cpp
+++ Arc/src/Arc/Core/Application.cpp
@@ -56,9 +56,9 @@ namespace ArcEngine
 		dispatcher.Dispatch<WindowCloseEvent>(ARC_BIND_EVENT_FN(Application::OnWindowClose));
 		dispatcher.Dispatch<WindowResizeEvent>(ARC_BIND_EVENT_FN(Application::OnWindowResize));
 
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
 		{
-			(*--it)->OnEvent(e);
+			(*it)->OnEvent(e);
 			if (e.Handled)
 				break;
 		}
