4257b2e2b078518197f93cb199c3d5920c40e497
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-28 03:26:03 +0530

    Added UI to add/remove entities and components + fixed camera bug When added during runtime

--- Arc-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Arc-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -29,12 +29,44 @@ namespace ArcEngine
 
 		if(ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
 			m_SelectionContext = {};
+
+		if(ImGui::BeginPopupContextWindow(0, 1, false))
+		{
+			if(ImGui::MenuItem("Create Empty Entity"))
+				m_Context->CreateEntity("Empty Entity");
+
+			ImGui::EndPopup();
+		}
+		
 		ImGui::End();
+		
 
 		
 		ImGui::Begin("Properties");
 		if(m_SelectionContext)
+		{
 			DrawComponents(m_SelectionContext);
+
+			if (ImGui::Button("Add Component"))
+				ImGui::OpenPopup("Add Component");
+
+			if (ImGui::BeginPopup("Add Component"))
+			{
+				if (ImGui::MenuItem("Camera"))
+				{
+					m_SelectionContext.AddComponent<CameraComponent>();
+					ImGui::CloseCurrentPopup();
+				}
+
+				if (ImGui::MenuItem("Sprite Renderer"))
+				{
+					m_SelectionContext.AddComponent<SpriteRendererComponent>();
+					ImGui::CloseCurrentPopup();
+				}
+				
+				ImGui::EndPopup();
+			}
+		}
 		ImGui::End();
 	}
 
@@ -47,9 +79,25 @@ namespace ArcEngine
 		if(ImGui::IsItemClicked())
 			m_SelectionContext = entity;
 
+		bool entityDeleted = false;
+		if(ImGui::BeginPopupContextItem())
+		{
+			if(ImGui::MenuItem("Delete Entity"))
+				entityDeleted = true;
+
+			ImGui::EndPopup();
+		}
+		
 		if(opened)
 		{
 			ImGui::TreePop();
+		}
+
+		if(entityDeleted)
+		{
+			m_Context->DestroyEntity(entity);
+			if(m_SelectionContext == entity)
+				m_SelectionContext = {};
 		}
 	}
 
@@ -123,9 +171,11 @@ namespace ArcEngine
 				tag = std::string(buffer);
 		}
 
+		const ImGuiTreeNodeFlags treeNodeFlags = ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_AllowItemOverlap;
+		
 		if(entity.HasComponent<TransformComponent>())
 		{
-			if(ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			if(ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), treeNodeFlags, "Transform"))
 			{
 				auto& transformComponent = entity.GetComponent<TransformComponent>();
 				DrawVec3Control("Translation", transformComponent.Translation);
@@ -137,10 +187,10 @@ namespace ArcEngine
 				ImGui::TreePop();
 			}
 		}
-
+		
 		if(entity.HasComponent<CameraComponent>())
 		{
-			if(ImGui::TreeNodeEx((void*)typeid(CameraComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Camera"))
+			if(ImGui::TreeNodeEx((void*)typeid(CameraComponent).hash_code(), treeNodeFlags, "Camera"))
 			{
 				auto& cameraComponent = entity.GetComponent<CameraComponent>();
 				auto& camera = cameraComponent.Camera;
@@ -205,13 +255,35 @@ namespace ArcEngine
 
 		if(entity.HasComponent<SpriteRendererComponent>())
 		{
-			if(ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Sprite Renderer"))
+			ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2{ 4, 4 });
+			bool open = ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), treeNodeFlags, "Sprite Renderer");
+			
+			ImGui::SameLine(ImGui::GetWindowWidth() - 25.0f);
+			if(ImGui::Button("+", ImVec2{ 20, 20 }))
+			{
+				ImGui::OpenPopup("ComponentSettings");
+			}
+			ImGui::PopStyleVar();
+
+			bool removeComponent = false;
+			if(ImGui::BeginPopup("ComponentSettings"))
+			{
+				if(ImGui::MenuItem("Remove Component"))
+					removeComponent = true;
+				
+				ImGui::EndPopup();
+			}
+			
+			if(open)
 			{
 				auto& spriteRenderer = entity.GetComponent<SpriteRendererComponent>();
 				ImGui::ColorEdit4("Color", glm::value_ptr(spriteRenderer.Color));
 
 				ImGui::TreePop();
 			}
+
+			if(removeComponent)
+				entity.RemoveComponent<SpriteRendererComponent>();
 		}
 	}
 }
--- Arc/src/Arc/Scene/Scene.h
+++ Arc/src/Arc/Scene/Scene.h
@@ -15,9 +15,13 @@ namespace ArcEngine
 		~Scene();
 
 		Entity CreateEntity(const std::string& name = std::string());
+		void DestroyEntity(Entity entity);
 
 		void OnUpdate(Timestep ts);
 		void OnViewportResize(uint32_t width, uint32_t height);
+	private:
+		template<typename T>
+		void OnComponentAdded(Entity entity, T& component);
 	private:
 		entt::registry m_Registry;
 		uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
