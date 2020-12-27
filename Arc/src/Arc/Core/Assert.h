31a942a24e93bd70e3f091a27b13bebea1e56fbd
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-28 01:16:57 +0530

    Replace filename extraction with equivalent standard library function

--- Arc/src/Arc/Core/Assert.h
+++ Arc/src/Arc/Core/Assert.h
@@ -3,29 +3,15 @@
 #include "Arc/Core/Base.h"
 #include "Arc/Core/Log.h"
 
-#ifdef ARC_ENABLE_ASSERTS
+#include <filesystem>
 
-	namespace ArcEngine::Assert
-	{
-		constexpr const char* CurrentFileName(const char* path)
-		{
-			const char* file = path;
-				while (*path)
-				{
-					if (*path == '/' || *path == '\\')
-						file = ++path;
-					else
-						path++;
-				}
-				return file;
-			}
-	}
+#ifdef ARC_ENABLE_ASSERTS
 
 	// Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
 	// provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
 	#define ARC_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { ARC##type##ERROR(msg, __VA_ARGS__); ARC_DEBUGBREAK(); } }
 	#define ARC_INTERNAL_ASSERT_WITH_MSG(type, check, ...) ARC_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-	#define ARC_INTERNAL_ASSERT_NO_MSG(type, check) ARC_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", ARC_STRINGIFY_MACRO(check), ::ArcEngine::Assert::CurrentFileName(__FILE__), __LINE__)
+	#define ARC_INTERNAL_ASSERT_NO_MSG(type, check) ARC_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", ARC_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 	#define ARC_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 	#define ARC_INTERNAL_ASSERT_GET_MACRO(...) ARC_EXPAND_MACRO( ARC_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, ARC_INTERNAL_ASSERT_WITH_MSG, ARC_INTERNAL_ASSERT_NO_MSG) )
