b8f25c33949ecea6d1616ab2af780e65f02d9594
Mohit Sethi <mohitsethi.32@gmail.com>
2020-12-27 15:31:25 +0530

    replaced std::bind in ARC_BIND_EVENT_FN macro with lambda

--- Arc/src/Arc/Core/Base.h
+++ Arc/src/Arc/Core/Base.h
@@ -57,7 +57,7 @@
 
 #define BIT(x) (1 << x)
 
-#define ARC_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define ARC_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 // Resolve which function signature macro will be used. Note that this only
 // is resolved when the (pre)compiler starts, so the syntax highlighting
