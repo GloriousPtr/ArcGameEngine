8bd015af5542d984079f98a29ec484e898e5c0ef
Mohit Sethi <mohitsethi.32@gmail.com>
2022-08-09 11:51:15 +0530

    Use Nvidia/AMD graphics card if available

--- Arc/src/Arc/Core/Application.cpp
+++ Arc/src/Arc/Core/Application.cpp
@@ -7,6 +7,12 @@
 #include <GLFW/glfw3.h>
 #include <optick.config.h>
 
+extern "C"
+{
+	__declspec(dllexport) DWORD NvOptimusEnablement = 1;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 namespace ArcEngine
 {
 	Application* Application::s_Instance = nullptr;
