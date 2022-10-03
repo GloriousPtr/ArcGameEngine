faec330ce3e6db6a51c4e24722b7362919810365
Mohit Sethi <mohitsethi.32@gmail.com>
2022-10-03 13:18:43 +0530

    [Compiler] Win-GenProjects.bat improvements

--- scripts/Win-GenProjects.bat
+++ scripts/Win-GenProjects.bat
@@ -2,11 +2,23 @@
 pushd %~dp0\..\
 
 set "compiler=%1%"
-if "%compiler%" == "" set /p compiler= Enter Compiler: 
-if "%compiler%" == "clang" set "compiler=--cc=clang"
-if "%compiler%" == "msvc" set "compiler="
+if "%compiler%" == "" set /p compiler= Enter Compiler (msvc/clang): 
 
-call vendor\premake\bin\premake5.exe %compiler% vs2022
+echo:
+echo ================================================================
+echo Generating Solution and project files for %compiler%
+echo ================================================================
+echo:
+
+
+set "compilerCommand=none"
+if "%compiler%" == "clang" set "compilerCommand=--cc=clang"
+if "%compiler%" == "msvc" set "compilerCommand="
+
+echo ================================================================
+if "%compilerCommand%" == "none" (echo Unsupported compiler %compiler%) else (call vendor\premake\bin\premake5.exe %compilerCommand% vs2022)
+echo ================================================================
+echo:
 
 popd
 PAUSE
