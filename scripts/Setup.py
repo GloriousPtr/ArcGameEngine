f77d57884055467c29ee90c25b56c296e6bf591c
Mohit Sethi <mohitsethi.32@gmail.com>
2023-09-09 22:08:05 +0530

    [Misc] Better setup.py

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -3,40 +3,42 @@ import pathlib
 import sys
 import subprocess
 
-compiler = "msvc"
+def GenProject(compiler):
+	if compiler == "clang" or compiler == "llvm":
+		subprocess.call("GenerateSolutionClang.bat", stdin=subprocess.DEVNULL)
+	else:
+		subprocess.call("GenerateSolution.bat", stdin=subprocess.DEVNULL)
+
+	if compiler == "llvm":
+		llvmInstallPath = "C:/Program Files/LLVM/bin/clang.exe"
+		if (not os.path.exists(llvmInstallPath)):
+			print("LLVM installation not found!")
+			subprocess.call("Install-LLVM.bat", stdin=subprocess.DEVNULL)
+		subprocess.call("Install-LLVM-Utils.bat", stdin=subprocess.DEVNULL)
+		for dname, dirs, files in os.walk("./.."):
+			for fname in files:
+				fpath = os.path.join(dname, fname)
+				extension = pathlib.Path(fpath).suffix
+				if extension != ".vcxproj":
+					continue
+				with open(fpath) as f:
+					s = f.read()
+				s = s.replace("<PlatformToolset>ClangCL</PlatformToolset>", "<PlatformToolset>LLVM_v143</PlatformToolset>")
+				with open(fpath, "w") as f:
+					f.write(s)
+	print("Generated project files for", compiler)
+
+cc = ""
 argsLen = len(sys.argv)
 if argsLen == 1:
-	compiler = input("Select compiler: msvc/clang/llvm ")
+	cc = input("Select compiler: msvc/clang/llvm ")
 elif argsLen == 2:
-	compiler = sys.argv[1]
+	cc = sys.argv[1]
 else:
 	print("Invalid syntax, should only contain one argument: msvc/clang/llvm")
-	exit
 
-if compiler != "msvc" and compiler != "clang" and compiler != "llvm":
-	print("Invalid compiler:", compiler)
-	exit
-
-if compiler == "clang" or compiler == "llvm":
-	subprocess.call("GenerateSolutionClang.bat", stdin=subprocess.DEVNULL)
+if cc != "msvc" and cc != "clang" and cc != "llvm":
+	print("Invalid compiler:", cc, "Using default (msvc) instead!")
+	GenProject("msvc")
 else:
-	subprocess.call("GenerateSolution.bat", stdin=subprocess.DEVNULL)
-
-if compiler == "llvm":
-	llvmInstallPath = "C:/Program Files/LLVM/bin/clang.exe"
-	if (not os.path.exists(llvmInstallPath)):
-		print("LLVM installation not found!")
-		subprocess.call("Install-LLVM.bat", stdin=subprocess.DEVNULL)
-	subprocess.call("Install-LLVM-Utils.bat", stdin=subprocess.DEVNULL)
-	for dname, dirs, files in os.walk("./.."):
-		for fname in files:
-			fpath = os.path.join(dname, fname)
-			extension = pathlib.Path(fpath).suffix
-			if extension != ".vcxproj":
-				continue
-			with open(fpath) as f:
-				s = f.read()
-			s = s.replace("<PlatformToolset>ClangCL</PlatformToolset>", "<PlatformToolset>LLVM_v143</PlatformToolset>")
-			with open(fpath, "w") as f:
-				f.write(s)
-print("Generated project files for", compiler)
+	GenProject(cc)
